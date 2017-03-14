8e17e98481e20e609f73af68c073e4326647e8e7
CXO <CXO@CXO-PC>
2017-03-14 15:55:07 -0300

    Add project files.

---
+++ SingnalRExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SingnalRExample.MvcApplication" Language="C#" %>
---
+++ SingnalRExample/Startup.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+[assembly: OwinStartup(typeof(SingnalRExample.Startup))]
+
+namespace SingnalRExample
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
